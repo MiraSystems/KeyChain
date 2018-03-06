88e538eb2a29b818951d6872b0ea5aa36d328c86
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-03-06 18:12:41 +0100

    changed js add

--- src/js/main.js
+++ src/js/main.js
@@ -29,7 +29,18 @@ let applications = [
 applications.forEach(function (app) {
 switch (app){
   case app === applications :
-  //make a new list item with this name
+    if (app !==// niet bestaat)
+    //make a new list item with this name
+      else{
+    let duplicate = prompt("This app already exists, do you want to duplicate?");
+    if (duplicate = "yes")
+    {
+      app + "Duplicate"//make a new list item with this name
+    }
+
+  }
+
+    //make a new list item with this name
     break;
   case app !== applications :
     let include = prompt("Do you want to add this application to the list?");
