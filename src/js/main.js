e9d238e01a854adfb8e819cd1f9e38acd04aa316
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-03-06 18:01:58 +0100

    applications list made and beginning .js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,45 @@
 // JS
+let applications = [
+  "Google",
+  "Facebook",
+  "Artesis Plantyn",
+  "Paybpal",
+  "Coolblue",
+  "One+1",
+  "VTM",
+  "KAVO",
+  "AdminBox",
+  "Ubuntu",
+  "Slack",
+  "Oracle",
+  "A veloke",
+  "Github",
+  "Steam",
+  "Discord",
+  "Twitch",
+  "Cisco",
+  "Amazon",
+  "Ali-expres",
+  "Reddit",
+  "Swarm",
+  "Belfius",
+  "Outlook",
+  "Office365"
+  ];
+applications.forEach(function (app) {
+switch (app){
+  case app === applications :
+  //make a new list item with this name
+    break;
+  case app !== applications :
+    let include = prompt("Do you want to add this application to the list?");
+    if (include = "yes")
+    {
+      applications.unshift(app);
+      //make a new list item with this name
+    }
+    else{
+      app.clear()
+    }
+}
+});
