9ac24e268f49a021308928d76968d7bd7799ae43
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-03-07 14:27:36 +0100

    answer add js

--- src/js/main.js
+++ src/js/main.js
@@ -11,13 +11,21 @@ function findApp (element) {
   let app = element.value;
   if (applications.indexOf(app) !== -1) {
     console.log('App exits: ' + applications[applications.indexOf(app)]);
-    let list = document.getElementById('list');
-    let adding = document.getElementById('AddItem').innerHTML;
-    let addings = document.createElement('li');
-    list.appendChild(addings);
-    addings.innerHTML = adding;
-    console.log(addings);
+    let jup = prompt("Do you want to add " + app + " to your list?");
+    if (jup === "yes")
+    {
+      addApp();
+    }
   } else {
     console.log('App doesn\'t exist');
   }
+}
+function addApp() {
+  let list = document.getElementById('list');
+  let adding = document.getElementById('AddItem').innerHTML;
+  let addings = document.createElement('li');
+  list.appendChild(addings);
+  addings.innerHTML = adding;
+  console.log(addings);
+
 }
