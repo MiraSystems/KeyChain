db03b6989648b79f4355366a69edbc118cad288e
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-03-07 14:08:44 +0100

    Add apps

--- src/js/main.js
+++ src/js/main.js
@@ -3,64 +3,21 @@ let applications = [
   "Google",
   "Facebook",
   "Artesis Plantyn",
-  "Paybpal",
+  "Paypal",
   "Coolblue",
-  "One+1",
-  "VTM",
-  "KAVO",
-  "AdminBox",
-  "Ubuntu",
-  "Slack",
-  "Oracle",
-  "A veloke",
-  "Github",
-  "Steam",
-  "Discord",
-  "Twitch",
-  "Cisco",
-  "Amazon",
-  "AliExpress",
-  "Reddit",
-  "Swarm",
-  "Belfius",
-  "Outlook",
-  "Office365"
 ];
 
 function findApp (element) {
   let app = element.value;
   if (applications.indexOf(app) !== -1) {
     console.log('App exits: ' + applications[applications.indexOf(app)]);
+    let list = document.getElementById('list');
+    let adding = document.getElementById('AddItem').innerHTML;
+    let addings = document.createElement('li');
+    list.appendChild(addings);
+    addings.innerHTML = adding;
+    console.log(addings);
   } else {
     console.log('App doesn\'t exist');
   }
 }
-
-/* applications.forEach(function (app) {
-switch (app){
-  case app === applications :
-    if (app !==// niet bestaat)
-    //make a new list item with this name
-      else{
-    let duplicate = prompt("This app already exists, do you want to duplicate?");
-    if (duplicate = "yes")
-    {
-      app + "Duplicate"//make a new list item with this name
-    }
-
-  }
-
-    //make a new list item with this name
-    break;
-  case app !== applications :
-    let include = prompt("Do you want to add this application to the list?");
-    if (include = "yes")
-    {
-      applications.unshift(app);
-      //make a new list item with this name
-    }
-    else{
-      app.clear()
-    }
-}
-}); */
